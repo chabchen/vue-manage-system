4a6e2ab6e5d8289c484fa65f3ffb2ad70965e166
chenchaofei <chaofei.chen@capgemini.com>
2019-08-22 09:59:12 +0800

    ccf

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 
 export const fetchData = (query) => {
     return request({
-        url: 'http://127.0.0.1:8088/api/sysMenu/menus/list',
+        url: '/sysMenu/menuForTree',
         method: 'get',
         data: query
     })
